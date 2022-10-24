3d24db875dfda53755508ebad25dfc193926737c
JoeDoom <reyesj1209@gmail.com>
2022-10-24 00:17:42 -0700

    adds models, routes, and a gitignore

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+//schema to create user model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts',
+        }],
+        friends: [{
+            type: Schema.Type.ObjectId,
+            ref: 'User',
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+)
+
+//creating virtual to get the ammount of friends.
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+})
+
+//Initializing User model.
+const User = model('user', userSchema);
+
+
+//exporting User
+module.exports = User;
